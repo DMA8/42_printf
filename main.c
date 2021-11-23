fcb8145a5e9bd2f8a04c11d9e080e009eb51e435
DMA8 <belovskoe@gmail.com>
2021-11-23 22:02:09 +0300

    asd

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main()
 	ft_printf("Address is %p Hello! My firstname is %s my last name is %s I am %x years old\n", &b, "Jovanie", "Jorjo", 51014);
 	printf("Address is %p Hello! My firstname is %s my last name is %s I am %x years old\n", &b, "Jovanie", "Jorjo", 51014);
 	ft_printf("%s %c %d %i %u %x %X %p %%\n", "asd", 'k', -101, -101, -101, -101, -101, &a);
+	
 	printf("%s %c %d %i %u %x %X %p %%", "asd", 'k', -101, -101, -101, -101, -101, &a);
 
 }
